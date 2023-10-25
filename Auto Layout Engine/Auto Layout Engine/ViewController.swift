052b66dfc16813c46a55a3fe2ffcc10624ff7c84
levanloladze <levan.loladzew@gmail.com>
2023-10-26 03:10:01 +0400

    almost done

--- Auto Layout Engine/Auto Layout Engine/ViewController.swift
+++ Auto Layout Engine/Auto Layout Engine/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lButton: UIButton!
+    @IBOutlet weak var mButton: UIButton!
+    @IBOutlet weak var sButton: UIButton!
+    
+    
+    var coffee: Coffee?
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lButton.layer.borderWidth = 1.0
+        lButton.layer.borderColor = UIColor.black.cgColor
+        
+        mButton.layer.borderWidth = 1.0
+        mButton.layer.borderColor = UIColor.black.cgColor
+        
+        sButton.layer.borderWidth = 1.0
+        sButton.layer.borderColor = UIColor.black.cgColor
+
+    }
+    
+    struct Coffee {
+        var productName: String
+        var price: Double
+        var rating
     }
 
 
